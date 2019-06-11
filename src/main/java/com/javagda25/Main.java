0c5e7fc2791ff7826eb31828ef7d211900afb998
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-06-11 18:12:12 +0200

    adding reader

--- src/main/java/com/javagda25/Main.java
+++ src/main/java/com/javagda25/Main.java
@@ -1,7 +1,25 @@
 package com.javagda25;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Scanner scanner = new Scanner(System.in);
+
+
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));) {
+            //System.out.println("Dopisz coś do pliku");
+            printWriter.println("Hello world!");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
 
 
