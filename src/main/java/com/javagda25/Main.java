953dd5bf8fc8932138794ebbac09d78816cd3469
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-06-11 18:27:22 +0200

    read file

--- src/main/java/com/javagda25/Main.java
+++ src/main/java/com/javagda25/Main.java
@@ -1,9 +1,35 @@
 package com.javagda25;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = null;
+        int howMuch = 0;
 
+        File plik = new File("data.txt");
+        if (plik.exists()) {
+            try {
+                scanner = new Scanner(new FileReader("data.txt"));
 
+                if (!scanner.hasNextLine()) {
+                    System.out.println("brak treeści");
+                }
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    howMuch++;
+                }
+
+            } catch (FileNotFoundException e) {
+                e.getLocalizedMessage();
+            }
+
+        }
+        System.out.println("linie: "+howMuch);
     }
 }
