3d1a97938af7688cc70b4793f229b3058d9c49eb
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-06-11 18:40:18 +0200

    editting

--- src/main/java/com/javagda25/Main.java
+++ src/main/java/com/javagda25/Main.java
@@ -7,28 +7,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         int howMuch = 0;
-
         File plik = new File("data.txt");
         if (plik.exists()) {
             try(Scanner scanner = new Scanner(new FileReader("data.txt"))) {
-
-
                 if (!scanner.hasNextLine()) {
                     System.out.println("brak treeści");
                 }
-
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
                     howMuch++;
                 }
-
             } catch (FileNotFoundException e) {
                 e.getLocalizedMessage();
             }
-
         }
         System.out.println("linie: " + howMuch);
     }
